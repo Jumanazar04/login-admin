b898d0a9a73060d0b564e3474a427e5584bf5d80
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-05-18 10:48:38 +0500

    fix: create-new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import React, { Suspense } from 'react';
+const Login = React.lazy(() => import('./pages/Login'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter >
+      <Suspense>
+        <Routes>
+          <Route path='/login' element={Login}/>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  )
 }
 
 export default App;
